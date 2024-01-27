3d9180685ad415a2e3db041d624b13d7509f8570
IshaRode <you@example.com>
2024-01-27 15:37:36 +0530

    Added thunk

---
+++ thunk/src/redux/reducer.jsx
@@ -0,0 +1,13 @@
+const initialState = {
+    data: []
+}
+
+export const reducer = (state = initialState, {type, payload}) => {
+    switch(type) {
+        case "fetchData":
+            return {...state, data: payload};
+        default:
+            return state;    
+
+    }
+}
